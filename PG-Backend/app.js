da927701b30c6df4bee6eb07f431b042d11a3f7a
Gaurav Juneja <gjuneja555@gmail.com>
2024-10-16 22:13:48 +0530

    live

--- PG-Backend/app.js
+++ PG-Backend/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // MIDDLEWARES
 // app.use(multerParse.none());
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://property-grab.vercel.app/"],
   credentials: true,
 };
 app.use(cors(corsOptions));
